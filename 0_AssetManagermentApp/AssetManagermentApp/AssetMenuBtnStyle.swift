36efa1c635a5c3f166ce8fec8d62d8d726c1c5ab
iammiori <immioriii@gmail.com>
2021-12-29 23:17:55 +0900

    [feat] 에셋메뉴버튼 entity구현

---
+++ 0_AssetManagermentApp/AssetManagermentApp/AssetMenuBtnStyle.swift
@@ -0,0 +1,20 @@
+//
+//  AssetMenuBtnStyle.swift
+//  AssetManagermentApp
+//
+//  Created by miori Lee on 2021/12/29.
+//
+
+import SwiftUI
+
+struct AssetMenuBtnStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AssetMenuBtnStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetMenuBtnStyle()
+    }
+}
