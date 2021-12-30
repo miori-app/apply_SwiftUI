095a6b046b315dc87d5b2b2944545156345bdb84
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2021-12-30 11:22:36 +0900

    [feat] buttonstyle 구현 (#4)

--- 0_AssetManagermentApp/AssetManagermentApp/AssetMenuBtnStyle.swift
+++ 0_AssetManagermentApp/AssetManagermentApp/AssetMenuBtnStyle.swift
@@ -7,14 +7,47 @@
 
 import SwiftUI
 
-struct AssetMenuBtnStyle: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct AssetMenuBtnStyle: ButtonStyle {
+    let menu : AssetMenu
+    
+    func makeBody(configuration: Configuration) -> some View {
+        return VStack {
+            Image(systemName: menu.systemImageName)
+                .resizable()
+                .frame(width: 30, height: 30, alignment: .center)
+                .padding([.leading,.trailing],10)
+            Text(menu.title)
+                .font(.system(size: 12, weight: .bold))
+        }
+        .padding(10)
+        .foregroundColor(.blue)
+        .background(Color.white)
+        //전체모양 오려내듯이
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
 struct AssetMenuBtnStyle_Previews: PreviewProvider {
     static var previews: some View {
-        AssetMenuBtnStyle()
+        // 수평
+        HStack(spacing : 24) {
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuBtnStyle(menu: .creditScore))
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuBtnStyle(menu: .bankAccount))
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuBtnStyle(menu: .investment))
+            Button("") {
+                print("")
+            }
+            .buttonStyle(AssetMenuBtnStyle(menu: .loan))
+        }
+        .background(Color.gray.opacity(0.2))
     }
 }
