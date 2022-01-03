c0d48a2cf448236e38958b3289c39aabca2a2ac9
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-01-03 23:10:32 +0900

    Assemble asset view (#6)
    * [feat] scroll view 안에 vstack있는 assetview 구현
    
    * [feat] 미리 만들어둔 navigationBarButtonStyle 합체

---
+++ 0_AssetManagermentApp/AssetManagermentApp/AssetView.swift
@@ -0,0 +1,30 @@
+//
+//  AssetView.swift
+//  AssetManagermentApp
+//
+//  Created by miori Lee on 2022/01/03.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 30) {
+                    Spacer()
+                    AssetMenuGridView()
+                }
+            }
+            // 미리 만들어둔 네비게이션 합체
+            .background(.gray.opacity(0.2))
+            .navigationBarWithBtnStyle("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
