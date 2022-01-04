88cb3dc9663a7a132e4523d418f7726bacf93e01
iammiori <immioriii@gmail.com>
2022-01-04 23:54:22 +0900

    [feat] pageViewController 추가

---
+++ 0_AssetManagermentApp/AssetManagermentApp/AssetBannerView.swift
@@ -0,0 +1,20 @@
+//
+//  AssetBannerView.swift
+//  AssetManagermentApp
+//
+//  Created by miori Lee on 2022/01/04.
+//
+
+import SwiftUI
+
+struct AssetBannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AssetBannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetBannerView()
+    }
+}
