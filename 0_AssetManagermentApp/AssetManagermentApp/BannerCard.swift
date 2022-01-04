4abb574664d73bd3ed992b7531b8fe81d4501e43
iammiori <immioriii@gmail.com>
2022-01-04 22:52:21 +0900

    [feat] banner에 쓰일 entity

---
+++ 0_AssetManagermentApp/AssetManagermentApp/BannerCard.swift
@@ -0,0 +1,20 @@
+//
+//  BannerCard.swift
+//  AssetManagermentApp
+//
+//  Created by miori Lee on 2022/01/04.
+//
+
+import SwiftUI
+
+struct BannerCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BannerCard_Previews: PreviewProvider {
+    static var previews: some View {
+        BannerCard()
+    }
+}
