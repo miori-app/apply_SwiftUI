b4efefc9660244139cbd9e1f1ad6c6ff8892d870
iammiori <immioriii@gmail.com>
2022-01-07 00:30:51 +0900

    [feat] secion Header View 따로 구현

---
+++ 0_AssetManagermentApp/AssetManagermentApp/AssetSectionView.swift
@@ -0,0 +1,20 @@
+//
+//  AssetSectionView.swift
+//  AssetManagermentApp
+//
+//  Created by miori Lee on 2022/01/06.
+//
+
+import SwiftUI
+
+struct AssetSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AssetSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetSectionView()
+    }
+}
