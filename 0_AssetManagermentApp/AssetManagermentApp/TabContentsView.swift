ff5c44c665429ab150ee594d850a4cdd5fc1e6e0
iammiori <immioriii@gmail.com>
2022-01-07 23:08:50 +0900

    [feat] Hstack과 Vstack활용해 탭 처럼 구현

---
+++ 0_AssetManagermentApp/AssetManagermentApp/TabContentsView.swift
@@ -0,0 +1,20 @@
+//
+//  TabContentsView.swift
+//  AssetManagermentApp
+//
+//  Created by miori Lee on 2022/01/07.
+//
+
+import SwiftUI
+
+struct TabContentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabContentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabContentsView()
+    }
+}
