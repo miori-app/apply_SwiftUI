df6b3c45798e114144cc60d19aefa3313330252a
iammiori <immioriii@gmail.com>
2022-01-07 23:09:43 +0900

    [feat] 탭 콘텐츠뷰 구성후 누른 탭에 맞게끔 콘텐츠 변경

--- 0_AssetManagermentApp/AssetManagermentApp/TabContentsView.swift
+++ 0_AssetManagermentApp/AssetManagermentApp/TabContentsView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct TabContentsView: View {
+    @State private var selectedTab : Int = 1
+    @ObservedObject var asset : AssetData
+    
+    var assetData : [AssetDataArr] {
+        // 이거만 뽑아쓸거야
+        return asset.data
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            AssetSectionHeaderView(title: asset.type.title)
+            TabMenuView(tabs: ["지난달 결제", "이번달 결제", "다음달 결제"], selectedTab: $selectedTab, updated: .constant(.nextMonth(amount: "10,000")))
+            TabView(selection: $selectedTab) {
+                ForEach(0...2, id:\.self){ i in
+                    VStack {
+                        ForEach(assetData) { data in
+                            HStack {
+                                Text(data.title)
+                                    .font(.title)
+                                    .foregroundColor(.secondary)
+                                Spacer()
+                                Text(data.creditCardAmounts![i].amount)
+                                    .font(.title3)
+                                    .foregroundColor(.primary)
+                            }
+                            Divider()
+                        }
+                    }
+                    .tag(i)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        }
+        .padding()
     }
 }
 
 struct TabContentsView_Previews: PreviewProvider {
     static var previews: some View {
-        TabContentsView()
+        let asset = AssetSummaryData().assets[5]
+        TabContentsView(asset: asset)
     }
 }
