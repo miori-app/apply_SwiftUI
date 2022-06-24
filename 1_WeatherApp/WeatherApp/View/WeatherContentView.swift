d6b9e841b1096426ac0eee5a910cd0b451fa0592
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-06-25 01:09:43 +0900

    #18 refactor: 뷰의 유지보수를 위해 기능별로 쪼개기 (#19)
    * #18 refactor: 뷰를 유지보수 하기 쉽게 쪼개기
    
    * #18 feat: 다크모드 설정

---
+++ 1_WeatherApp/WeatherApp/View/WeatherContentView.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherContentView.swift
+//  WeatherApp
+//
+//  Created by miori Lee on 2022/06/25.
+//
+
+import SwiftUI
+
+struct WeatherContentView: View {
+    var body: some View {
+        VStack {
+            Text("잠실역")
+                .font(.title2)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 60)
+            
+            GeometryReader { reader in
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        Color.red // 나머지 높이
+                        CurrentWeatherView() //실제 내용
+                    }
+                    .frame(height: reader.size.height)
+                    
+                    Spacer(minLength: reader.safeAreaInsets.bottom)
+                    
+                    ForecastView() //예보부분
+                     
+                }
+            }
+        }
+    }
+}
+
+struct WeatherContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherContentView()
+            .preferredColorScheme(.dark)
+    }
+}
