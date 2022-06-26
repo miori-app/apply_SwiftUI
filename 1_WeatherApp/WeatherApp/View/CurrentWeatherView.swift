964d6eba65b8bf1d50783e9a83d5a8f54e6ffaad
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-06-26 23:45:24 +0900

    feat: model 데이터  view 에 뿌려주기 (#20)
    * feat: 예보 뷰에 프리뷰 데이터 뿌리기
    
    * feat: 현재 날씨뷰와 모델 데이터 이어주기

--- 1_WeatherApp/WeatherApp/View/CurrentWeatherView.swift
+++ 1_WeatherApp/WeatherApp/View/CurrentWeatherView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct CurrentWeatherView: View {
+    let model : CurrentWeather
+    
     var body: some View {
-        Color.yellow
-            .frame(height: 200)
+        VStack(alignment: .leading) {
+            //날씨이미지, 날씨 텍스트 수평
+            HStack {
+                Image(systemName: model.icon)
+                Text(model.weather)
+            }
+            .font(.largeTitle)
+            
+            HStack(spacing: 20) {
+                Label(model.maxTemperature, systemImage: "arrow.up")
+                Label(model.minTemperature, systemImage: "arrow.down")
+            }
+            
+            HStack(alignment: .lastTextBaseline) {
+                Text(model.temperature)
+                    .font(.system(size: 120,weight: .ultraLight))
+                .minimumScaleFactor(0.5)
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "sunrise.fill")
+                        .symbolRenderingMode(.multicolor)
+                    Text(model.sunrise)
+                    Image(systemName: "sunset.fill")
+                        .symbolRenderingMode(.multicolor)
+                    Text(model.sunset)
+                }
+                .font(.caption)
+            }
+        }
+        .foregroundColor(.white)
+        .padding(.horizontal)
     }
 }
 
 struct CurrentWeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        CurrentWeatherView()
+        CurrentWeatherView(model: .preview)
+            .preferredColorScheme(.dark)
     }
 }
