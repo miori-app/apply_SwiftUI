0ad161cb53805ee4017860d10940b6f9b2a68c56
Mi Yeon Lee <immioriii@gmail.com>
2022-07-06 23:26:18 +0900

    #25 feat: view- listview 생성후 navigationview에 임베드, 저장데이터 뿌려주기

--- WeightliftingLog/Shared/MainListView.swift
+++ WeightliftingLog/Shared/MainListView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store : TrainingLogStore
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { trLog in
+                VStack(alignment: .leading) {
+                    Text(trLog.trainingLog)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text(trLog.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("역도기록")
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(TrainingLogStore())
     }
 }
