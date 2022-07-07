c34bf2a54a334ded22dc9f4363bc0410e938c4de
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-07-08 01:33:06 +0900

    #28 feat: 기록 작성뷰 - 네비게이션 툴바버튼 구현 (#29)
    * #28 refac: 메인리스트뷰를 뷰그룹으로 이동
    
    * #28 feat: 기록작성뷰에서 네비게이션뷰 구현(왼쪽,오른쪽 버튼)

---
+++ WeightliftingLog/Shared/View/ComposeView.swift
@@ -0,0 +1,52 @@
+//
+//  ComposeView.swift
+//  WeightliftingLog
+//
+//  Created by miori Lee on 2022/07/08.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store: TrainingLogStore
+    @Environment(\.dismiss) var dismiss
+    //입력한 텍스트 바인딩
+    //state variable
+    @State private var content : String = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                //투웨이 바인딩
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("새 일지")
+            .navigationBarTitleDisplayMode(.inline) //large Title 안써
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        //저장
+                        store.insert(log: content)
+                    } label: {
+                        Text("저장")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(TrainingLogStore())
+    }
+}
