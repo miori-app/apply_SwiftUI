efd2d43f2882d1b211c2f53a2de9bfe528341a5f
Mi Yeon Lee <immioriii@gmail.com>
2022-07-08 03:50:29 +0900

    #30 feat: 네비게이션바에 버튼 추가후, 화면이동 코드 추가

--- WeightliftingLog/Shared/View/MainListView.swift
+++ WeightliftingLog/Shared/View/MainListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MainListView: View {
     @EnvironmentObject var store : TrainingLogStore
-    
+    @State private var showCompseView: Bool = false
     var body: some View {
         NavigationView {
             List(store.list) { trLog in
@@ -17,6 +17,16 @@ struct MainListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("역도기록")
+            .toolbar {
+                Button {
+                    showCompseView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showCompseView) {
+                ComposeView()
+            }
         }
     }
 }
