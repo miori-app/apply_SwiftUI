a67020812f96d519a66ec57145f2419d7c7a70bb
Mi Yeon Lee <immioriii@gmail.com>
2022-07-10 23:05:47 +0900

    #36 feat: 삭제버튼 추가후 삭제기능 구현 + alert 으로 한번더 확인

--- WeightliftingLog/Shared/View/DetailView.swift
+++ WeightliftingLog/Shared/View/DetailView.swift
@@ -11,6 +11,8 @@ struct DetailView: View {
     @ObservedObject var trLog: TrainingLog
     @EnvironmentObject var store: TrainingLogStore
     @State var showComposeView: Bool = false
+    @State private var showDeleteAlert: Bool = false
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         VStack {
             ScrollView {
@@ -31,6 +33,24 @@ struct DetailView: View {
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showDeleteAlert = true
+                } label: {
+                    Image(systemName: "trash")
+                }
+                .foregroundColor(.red)
+                .alert("삭제 확인", isPresented: $showDeleteAlert) {
+                    Button(role: .destructive) {
+                        store.delete(log: trLog)
+                        dismiss()
+                    } label: {
+                        Text("삭제")
+                    }
+                } message: {
+                    Text("메모를 삭제할까요?")
+                }
+
+
                 Button {
                     showComposeView = true
                 } label: {
